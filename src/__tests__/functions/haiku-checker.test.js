24b5a7c06f7189c16e6a27018f8c3bf7aefa0710
Noel Kirkland <noelkirkland@gmail.com>
2020-10-14 14:35:26 -0600

    pass test for spec 4

--- src/__tests__/functions/haiku-checker.test.js
+++ src/__tests__/functions/haiku-checker.test.js
@@ -13,4 +13,8 @@ describe('haikuChecker', () => {
   test('should correctly count how many syllables are in a word with only one vowel as well as a silent e but no dipthongs', () => {
     expect(haikuChecker("code")).toEqual(1);
   });
+
+  test('should correctly count how many syllables are in a word with multiple vowels as well as a silent e but no dipthongs', () => {
+    expect(haikuChecker("complete")).toEqual(2);
+  });
 });
